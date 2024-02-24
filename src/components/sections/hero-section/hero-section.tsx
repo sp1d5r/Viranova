f8cceeb2f74d2b36b304d45d4e71fad8e00b2d6f
Elijah <aa1719@imperial.ac.uk>
2024-02-24 19:42:09 +0000

    Frontend: Redesigned hero section.

--- src/components/sections/hero-section/hero-section.tsx
+++ src/components/sections/hero-section/hero-section.tsx
@@ -1,34 +1,75 @@
 import React from 'react';
 import Background from "../../../assets/landing-page-assets/background.png";
-import { PrimaryButton } from '../../buttons/primary-button/primary-button';
-import { SecondaryButton } from '../../buttons/secondary-button/secondary-button';
-import Mouse from "../../../assets/icons/Mouse.svg";
+import PeaceLeft from "../../../assets/landing-page-assets/PeaceFingersLeft.svg";
+import PeaceRight from "../../../assets/landing-page-assets/PeaceFingersRight.svg";
+import Underline from "../../../assets/landing-page-assets/underline.svg";
+import DrivingVideo from "../../../assets/landing-page-assets/WatchingMovie.svg";
+import BuildingBlocks from "../../../assets/landing-page-assets/BuildingBlocks.svg";
+import LayingDown from "../../../assets/landing-page-assets/LayingDown.svg";
+import Segmentation from "../../../assets/landing-page-assets/Segmentation.svg";
 
 export interface HeroSectionProps {
     className?: string;
 }
 
+
+export interface LandingCardProps {
+    imageSrc: string;
+    cardTitle: string;
+    cardDescription: string;
+}
+const LandingCard : React.FC<LandingCardProps> = ({imageSrc, cardTitle, cardDescription}) => {
+    return <div
+        className={"flex-col bg-white/5 flex border-primary border h-72 backdrop-blur rounded-xl hover:scale-110 transition-all p-3 justify-center items-center w-[20%] gap-3 hover:shadow hover:shadow-primary"}>
+        <img src={imageSrc} alt={"landing card image"} className={"mix-blend-darken h-32 w-32 aspect-square"}/>
+        <p className={"font-bold"}>{cardTitle}</p>
+        <p>{cardDescription}</p>
+    </div>;
+}
+
 export const HeroSection: React.FC<HeroSectionProps> = ({ className = '' }) => (
-    <div className={className + 'container bg-background text-accent w-full min-h-[80vh] flex gap-5 justify-center items-center md:flex-wrap-reverse  px-5 flex-col-reverse sm:flex-row container'  }>
-        <div className={"h-full flex-1 min-w-[300px] flex flex-col gap-5"}>
-            <h1 className={"text-title"}>
-                <span>Go Viral on TikTok: Turn Lengthy Videos into: </span>
-                <span>Snappy Hits</span>
-            </h1>
-            <p>
-                Join the businesses working with us to improve engagement, increase retention and reduce time spent editing your videos!
-            </p>
-            <div className={"flex gap-3"}>
-                <PrimaryButton text={"Get Started"} icon="" onClick={(e)=>{window.location.href ="/playground"}} />
-                <SecondaryButton text={"Learn More"} icon="" onClick={(e) => {console.log("Here2")}}/>
-            </div>
-            <div className={"flex gap-3 items-center"}>
-                <img className={"h-[50px]"} src={Mouse} alt={"/"}/>
-                <p>Scroll to Continue </p>
+    <div
+        className={className + 'container bg-background text-white w-full min-h-[80vh] flex gap-5 justify-center items-center md:flex-wrap-reverse  px-5 flex-col-reverse sm:flex-row container'}>
+        <img className={"absolute top-10"} src={Background} alt={""}/>
+        <div className={"z-10 flex flex-col gap-10 justify-center"}>
+            <div className={"flex justify-center items-center relative "}>
+                <div className={"relative group"}>
+                    <div
+                        className={"absolute -inset-0.5 bg-gradient-to-r from-green-500 to-cyan-500 rounded-lg blur opacity-65 group-hover:-inset-1"}></div>
+                    <div
+                        className={"relative bg-background text-xl  px-5 py-2 rounded-xl group-hover:text-green-300 transition duration-200"}>
+                        <p>
+                            Powered by the Blockchain
+                        </p>
+                    </div>
+                </div>
             </div>
 
-            <img className={"absolute top-10"} src={Background} alt={""} />
 
+            <div className={"flex gap-5 justify-center"}>
+                <img className={"hidden md:flex h-20"} src={PeaceLeft} alt={"peace fingers left"}/>
+                <div>
+                    <span className={"text-title m-0 text-center relative"}>
+                        Block Chain Powered {" "}
+                        <span className={"text-primary relative"}>
+                            Computer Vision
+                            <img className={"absolute right-0 -bottom-0.5"} src={Underline} alt={"_"}/>
+                        </span>
+                    </span>
+                    <p>Step into a new era of business connections powered by AI, designed exclusively for the Web3
+                        domain.</p>
+                </div>
+                <img className={"hidden md:flex h-20"} src={PeaceRight} alt={"peace fingers right"}/>
+            </div>
+
+
+            <div className={"flex gap-5 justify-center"}>
+                <LandingCard imageSrc={DrivingVideo} cardTitle={"Driving Video"} cardDescription={"Provide the model with a driving video."}/>
+                <LandingCard imageSrc={Segmentation} cardTitle={"Segmentation"} cardDescription={"Perform Temporal and Spacial Segmentation."}/>
+                <LandingCard imageSrc={LayingDown} cardTitle={"Metric Driven"} cardDescription={"Fine-tune videos for your audience."}/>
+                <LandingCard imageSrc={BuildingBlocks} cardTitle={"Blockchain Powered"} cardDescription={"Inference and training powered by blockchain."}/>
+            </div>
         </div>
+
     </div>
 );
