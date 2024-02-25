6481276b540a44382be021cd48ad5d8a2f90c104
Elijah <aa1719@imperial.ac.uk>
2024-02-25 16:33:20 +0000

    Frontend: Updated sections to include progress bar.

--- src/components/sections/why-section/why-section.tsx
+++ src/components/sections/why-section/why-section.tsx
@@ -7,10 +7,10 @@ export interface WhySectionProps {
 
 
 export const WhySection: React.FC<WhySectionProps> = ({ className = '' }) => (
-    <div className={className + "container flex flex-col gap-5 min-h-[80vh] justify-center"}>
+    <section className={className + "container flex flex-col gap-5 min-h-[50vh] justify-center"}>
         <HiddenBackgroundTitle />
         <p className="text-bold text-primary">Generating TikTok Clips is not hard.</p>
         <p className="text-accent">You need to generate funny clips that engage your viewers, this means you need to give your videos personality. This lets you build a fan base of paying customers.</p>
         <p className="text-accent">This will bring people back to you helping you build that fanbase.</p>
-    </div>
+    </section>
 );
