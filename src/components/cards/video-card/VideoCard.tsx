b47a6600f420d7b8dbd13fbb3be0982c10400fdc
Elijah <aa1719@imperial.ac.uk>
2024-03-08 11:44:28 +0000

    Frontend: Updated segmentation intro space.

--- src/components/cards/video-card/VideoCard.tsx
+++ src/components/cards/video-card/VideoCard.tsx
@@ -3,10 +3,11 @@ import {Video} from "../../../types/Video";
 
 export interface VideoCardProps {
     video: Video;
+    className?: string;
 }
 
-export const VideoCard: React.FC<VideoCardProps> = ({video}) => {
-    return <div className={"flex flex-col py-5 flex-1  justify-evenly items-center gap-2 min-w-[200px] min-h-[400px] bg-white/5 hover:bg-white/10 rounded-xl backdrop-blur-sm"}>
+export const VideoCard: React.FC<VideoCardProps> = ({video, className=""}) => {
+    return <div className={`flex flex-col py-5 flex-1  justify-evenly items-center gap-2 min-w-[200px] min-h-[400px] bg-white/5 hover:bg-white/10 rounded-xl backdrop-blur-sm ${className}`}>
         <img className={"max-h-[200px] object-cover"} src={video ? video.thumbnailUrl :"https://placehold.jp/300x150.png"} alt={"something "} />
 
         <div className={"flex flex-col gap-1 items-start w-[80%]"}>
